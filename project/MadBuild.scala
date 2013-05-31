beb169b8e4b3b57e58306452e66b60fbbc831fca
fabse <fab@fabiankoehler.de>
2013-05-31 23:09:17 +0200

    new adync d version

--- project/MadBuild.scala
+++ project/MadBuild.scala
@@ -9,16 +9,14 @@ object MadBuild extends Build {
     settings = Project.defaultSettings ++ Seq(
       name := "mad",
       organization := "com.github.mad",
-      version := "0.2.11",
+      version := "0.2.12",
       scalaVersion := "2.10.1",
       libraryDependencies ++= Seq(
         "com.jsuereth" %% "scala-arm" % "1.3",
         "org.specs2" %% "specs2" % "1.14" % "test",
-        "com.allanbank" % "mongodb-async-driver" % "1.2.0",
         "joda-time" % "joda-time" % "2.2",
         "org.joda" % "joda-convert" % "1.3.1"
       ),
-      resolvers += "allanbank" at "http://www.allanbank.com/repo/",
       publishTo <<= version {
         (v: String) =>
           val base = "/Users/fab/dev/fkoehler-mvn-repo"
